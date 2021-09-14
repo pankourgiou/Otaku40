1a48f06c3db7bcbb9afd2722f6e1ccdfd6171cff
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-09-14 07:27:08 +0300

    Create Zeno.py

---
+++ Zeno.py
@@ -0,0 +1,6 @@
+import sys
+import time
+
+for index in range(60):
+    print("Run or walk with small steps or even in a circular fashion. Give a sense of movement no need to be an impressive one.it's a good exercise. The turtle will always be in front of Achilles even if Achilles goes full throtle.")
+    time.sleep(3)
