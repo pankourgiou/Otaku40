853a395c99f23220e28b1c89a15bfa344f432bb1
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-12-31 03:34:43 +0200

    Update Translate_Voltik.py

--- Translate_Voltik.py
+++ Translate_Voltik.py
@@ -1,4 +1,31 @@
 #Install translators module
+#The alphabet sounds from A-Z are:
+#ka
+#sa
+#ta
+#pa
+#ya
+#na
+#ha
+#ma
+#ra
+#za
+#la
+#wa
+#va
+#ba
+#ga
+#da
+#ja
+#fa
+#xa
+#ca
+#zu
+#du
+#ju
+#fu
+#gu
+#ku
 import translators as ts
 
 text = 'cheese'
